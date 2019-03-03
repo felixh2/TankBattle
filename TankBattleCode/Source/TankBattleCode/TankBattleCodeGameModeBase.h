cc91736b22ca81ac9b4761cc4e989c789cad1e09
felix don <felixh2@gmail.com>
2019-03-03 18:32:11 +0200

    first local commit with basic landscape

---
+++ TankBattleCode/Source/TankBattleCode/TankBattleCodeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankBattleCodeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKBATTLECODE_API ATankBattleCodeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
